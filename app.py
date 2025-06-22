eb870097841678004c7c86a604a0662d53b944ff
Estibanna <estibannatwitch@gmail.com>
2025-06-23 00:07:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "App is alive"
 
-if __name__ == '__main__':
-    app.run()
+app.run(host='0.0.0.0', port=81)
