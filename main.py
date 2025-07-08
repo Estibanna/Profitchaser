6b36a999aeed1a96eed9e1bd146609bfb1a1e461
Estibanna <estibannatwitch@gmail.com>
2025-07-08 07:42:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from datetime import datetime, timezone, timedelta
 def is_mod_or_owner(member):
     role_names = [role.name.lower() for role in member.roles]
     return "mods" in role_names or "owners" in role_names
-ALLOWED_WATCH_USERS = {"estibanna", "noltie"}  # usernames in kleine letters
+ALLOWED_WATCH_USERS = {"estibanna"}  # usernames in kleine letters
 TRIAL_FILE = "data/trials.json"
 TRIAL_ROLE_NAME = "Millionaire"
 
